8350646984939ad4b5d8d4cdf421b5bdcbaa7ffa
Tiberiu Covaci <tibi@covaci.se>
2018-05-28 12:52:07 +0100

    Add the Instructions and the Ingredients components

---
+++ src/app/ingredients/ingredients.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients',
+  templateUrl: './ingredients.component.html',
+  styleUrls: ['./ingredients.component.css']
+})
+export class IngredientsComponent implements OnInit {
+
+  @Input() ingredients: IRecipeIngredient[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
